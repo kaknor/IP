29d73568c6eae9a54c60272945404876f0246d66
Mortician <caradine.clyde@gmail.com>
2017-09-24 19:47:01 +0200

    [TM][NTMD][LAYOUT] still need to check properly

--- tmExtraction.py
+++ tmExtraction.py
@@ -33,63 +33,55 @@ def get_bounds(s):
     part = s.partition('-')
     return (int(part[0]), int(part[2]))
     
-
-# def get_topo(name, topo):
-#     tmd = []
-#     ntmd = [()]
-#     for i in range(0, len(topo)):
-#         if topo[i] == 'o':
-#             left, right = get_bounds(topo[i + 1:], 'i')
-#             if (left != '' and right != ''):
-#                 ntmd.append((int(left), int(right)))
-#             else:
-#                 tmd.append(((tmd[-1][1][1], general_file_data[name][0]), ()))
-#         if topo[i] == 'i':
-#             left, right = get_bounds(topo[i + 1:], 'o')
-#             if (left != '' and right != ''):
-#                 tmd.append((ntmd[-1], (int(left), int(right))))
-#             else:
-#                 tmd.append(((ntmd[-1]), (ntmd[-1][1], general_file_data[name][0])))
-#     return tmd, ntmd
-
-def get_topo(name, topo):
-    tmd = [()]
+def get_tmd(name, topo):
+    tmd = [()]# 0 = precedent lbound; 1 = suiv upbound; 2 = current lbound; 3 = current upbound
     for d in re.findall("[io]([0-9]*-[0-9]*)", topo):
         left, right = get_bounds(d)
         if tmd[-1] is ():
             tmd.append((1, left - 1, left, right))
         else:
-            tmd.append((tmd[-1][3] + 1, right - 1, left, right))
+            tmd.append((tmd[-1][3] + 1, left - 1, left, right))
     return tmd
 
 def get_prot_seq(d_fasta, name, bounds):
-    return d_fasta[name][bounds[0]:bounds[1]]
+    if general_file_data[name][0] == bounds[1]:
+        return d_fasta[name][bounds[0]:]
+    else:
+        return d_fasta[name][bounds[0]:bounds[1] + 1]
 
-def form_line(f, d_fasta, i, tmdom, ntmdom):
+def create_line(f, d_fasta, i, tmd):
     f.write(i[0] + '\t')
-    f.write(i[1].strip('\n'))
-    if tmdom:
-        f.write('\t' + str(tmdom[0]) + ' - ' + str(tmdom[1]) + '\t' + get_prot_seq(d_fasta, i[0], tmdom))
-    if ntmdom:
-        f.write('\t' + str(ntmdom[0]) + ' - ' + str(ntmdom[1]) + '\t' + get_prot_seq(d_fasta, i[0], ntmdom))
-    f.write('\n')
-            
+    f.write(i[1].strip('\n') + '\t')
+    f.write(str(tmd[2]) + ' - ' + str(tmd[3]) + '\t')
+    f.write(get_prot_seq(d_fasta, i[0], (tmd[2], tmd[3])) + '\t')
+    f.write(str(tmd[0]) + ' - ' + str(tmd[1]) + '\t')
+    f.write(get_prot_seq(d_fasta, i[0], (tmd[0], tmd[1])) + '\t')
+
+def create_last_ntmd(f, d_fasta, i, lbound):
+    l = general_file_data[i[0]][0]
+    f.write(i[0] + '\t')
+    f.write(i[1].strip('\n') + '\t')
+    f.write('N/A\t')
+    f.write('N/A\t')
+    seq = get_prot_seq(d_fasta, i[0], (lbound, l))
+    f.write(str(lbound + 1) + ' - ' + str(l)  + '\t')
+    f.write(seq + '\t')
+    
 def create_csv(file_name):
     d_fasta = dict(fasta_genes)
     with open('outputTopologyFiles/' + file_name + '_' + timestr + '.csv', 'w') as f:
         f.write('[ID]' + '\t' + '[TOPOLOGY]' + '\t' + '[TMD #]' + '\t' + '[TMD protein sequence]' + '\t'
                 + '[NON TMD #]' + '\t' + '[NON TMD protein sequence]\n')
         for i in topology:
-            tmd = get_topo(i[0], i[1])
-            print(tmd)
-            # for tmdom, ntmdom in zip(tmd, ntmd):
-            #     form_line(f, d_fasta, i, tmdom, ntmdom)
-            # if len(tmd) == len(ntmd):
-            #     continue
-            # if len(tmd) > len(ntmd):
-            #     form_line(f, d_fasta, i, tmd[-1], [])
-            # else:
-            #     form_line(f, d_fasta, i, [], ntmd[-1])
+            tmd = get_tmd(i[0], i[1])[1:]
+            for d in tmd:
+                create_line(f, d_fasta, i, d)
+                f.write('\n')
+            upbound = tmd[-1][3]
+            if general_file_data[i[0]][0] > upbound:
+                create_last_ntmd(f, d_fasta, i, upbound)
+                f.write('\n')
+
             
 def print_topology():
     for i in topology:
@@ -99,7 +91,7 @@ def print_fasta():
     for i in fasta_genes:
         print(i)
 
-#fasta_parser('liverStageGenes-wp')
-#topology_parser('TopologyPbLs')
-fasta_parser('testls')
-topology_parser('test')
+fasta_parser('liverStageGenes-wp')
+topology_parser('TopologyPbLs')
+# fasta_parser('testls')
+# topology_parser('test')
